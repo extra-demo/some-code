099f4de804b10cb3bdba6d7a4ef5e4f51016a0a9
霸气千秋 <minbaby.zhang@gmail.com>
2019-06-15 12:51:13 +0800

    update

--- c/005/main.c
+++ c/005/main.c
@@ -7,30 +7,30 @@
 #include "3rdparty/json/ujdecode.h"
 
 struct JRC_ORIGIN {
-    wchar_t *author;
-    wchar_t *title;
-    wchar_t **content;
-    wchar_t **translate;
-    wchar_t *dynasty;
+  wchar_t *author;
+  wchar_t *title;
+  wchar_t **content;
+  wchar_t **translate;
+  wchar_t *dynasty;
 
 };
 
 struct JRC_DATA {
-    struct JRC_ORIGIN *origin;
-    wchar_t *content;
-    wchar_t *id;
-    wchar_t **matchTags;
-    wint_t popularity;
-    wchar_t *recommendedReson;
-    wchar_t *cacheAt;
+  struct JRC_ORIGIN *origin;
+  wchar_t *content;
+  wchar_t *id;
+  wchar_t **matchTags;
+  wint_t popularity;
+  wchar_t *recommendedReson;
+  wchar_t *cacheAt;
 };
 
 struct JRC {
-    wchar_t *status;
-    struct JRC_DATA *data;
-    wchar_t *ipAddress;
-    wchar_t *warning;
-    wchar_t *token;
+  wchar_t *status;
+  struct JRC_DATA *data;
+  wchar_t *ipAddress;
+  wchar_t *warning;
+  wchar_t *token;
 };
 
 static void requestData();
@@ -40,51 +40,53 @@ static struct JRC decodeJson(const char *str, struct JRC *data);
 static wchar_t *getStrByType(UJObject obj);
 
 int main(int argc, char **argv) {
-    requestData();
-    return 0;
+//  requestData();
+  printf("你好\n");
+  return 0;
 }
 
 typedef struct {
-    size_t size;
-    char *buf;
+  size_t size;
+  char *buf;
 } _Buffer;
 
 static size_t write_cb(void *contents, size_t size, size_t nmemb, void *user_ptr) {
-    _Buffer *mem = (_Buffer *) user_ptr;
+  _Buffer *mem = (_Buffer *) user_ptr;
 
-    size_t buffer_size = size * nmemb;
+  size_t buffer_size = size * nmemb;
 
-    char *tmp = realloc(mem->buf, mem->size + buffer_size);
-    if (!tmp) {
-        printf("申请内存失败\n");
-        return 0;
-    }
+  char *tmp = realloc(mem->buf, mem->size + buffer_size);
+  if (!tmp) {
+    wprintf(L"申请内存失败\n");
+    return 0;
+  }
 
-    mem->buf = tmp;
-    memcpy(&(mem->buf[mem->size]), contents, buffer_size);
-    mem->size += buffer_size;
+  mem->buf = tmp;
+  memcpy(&(mem->buf[mem->size]), contents, buffer_size);
+  mem->size += buffer_size;
 
-    return buffer_size;
+  return buffer_size;
 }
 
 static struct JRC decodeJson(const char *str, struct JRC *data) {
-    void *state;
-    size_t cbInput = strlen(str);
+  void *state;
+  size_t cbInput = strlen(str);
 
-    struct JRC jrc;
+  struct JRC jrc;
 
     UJObject obj = UJDecode(str, cbInput, NULL, &state);
 
-    const wchar_t *keys[] = {L"status", L"data", L"ipAddress", L"warning", L"token"};
-    UJObject oState, oData, oIpAddress, oWarning, oToken;
+  const wchar_t *keys[] = {L"status", L"data", L"ipAddress", L"warning", L"token"};
+  UJObject oState, oData, oIpAddress, oWarning, oToken;
 
-    if (UJObjectUnpack(obj, 5, "SOSsS", keys, &oState, &oData, &oIpAddress, &oWarning, &oToken)) {
+
+  if (UJObjectUnpack(obj, 5, "SOSsS", keys, &oState, &oData, &oIpAddress, &oWarning, &oToken)) {
         UJObject o[] = {oState, oData, oIpAddress, oWarning, oToken};
         for (int i = 0; i < 5; i++) {
-            wprintf(
-                    L"\n=====> %ls: %ls : %ls<=====\n",
+    wprintf(
+        L"\n=====> %ls: %ls : %ls<=====\n",
                     keys[i],
-                    UJReadString(o[i], NULL),
+        UJReadString(o[i], NULL),
                     getStrByType(o[i])
             );
         }
@@ -110,8 +112,7 @@ static struct JRC decodeJson(const char *str, struct JRC *data) {
 
         jrc.data = oData;
     }
-
-    return jrc;
+  return jrc;
 }
 
 wchar_t *getStrByType(UJObject obj) {
@@ -145,35 +146,35 @@ wchar_t *getStrByType(UJObject obj) {
 }
 
 static void requestData() {
-    CURL *hd = curl_easy_init();
+  CURL *hd = curl_easy_init();
 
-    if (hd) {
-        curl_easy_setopt(hd, CURLOPT_URL, "https://v2.jinrishici.com/one.json");
+  if (hd) {
+    curl_easy_setopt(hd, CURLOPT_URL, "https://v2.jinrishici.com/one.json");
 
-        _Buffer *_this = malloc(sizeof(_Buffer));
-        _this->size = 0;
-        _this->buf = malloc(1);
-        curl_easy_setopt(hd, CURLOPT_WRITEFUNCTION, write_cb);
-        curl_easy_setopt(hd, CURLOPT_WRITEDATA, _this);
+    _Buffer *_this = malloc(sizeof(_Buffer));
+    _this->size = 0;
+    _this->buf = malloc(1);
+    curl_easy_setopt(hd, CURLOPT_WRITEFUNCTION, write_cb);
+    curl_easy_setopt(hd, CURLOPT_WRITEDATA, _this);
 
-        // struct curl_slist *list = NULL;
-        // list = curl_slist_append(list, "X-User-Token：XlKfjT4/eYOiehrAdEgwbipKIe6G9sbU");
+    // struct curl_slist *list = NULL;
+    // list = curl_slist_append(list, "X-User-Token：XlKfjT4/eYOiehrAdEgwbipKIe6G9sbU");
 
-        // curl_easy_setopt(hd, CURLOPT_HTTPHEADER, list);
+    // curl_easy_setopt(hd, CURLOPT_HTTPHEADER, list);
 
 //        curl_easy_setopt(hd, CURLOPT_VERBOSE, 1);
 
-        CURLcode ret = curl_easy_perform(hd);
+    CURLcode ret = curl_easy_perform(hd);
 
-        if (ret != CURLE_OK) {
-            fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(ret));
-        }
-
-        wprintf(L"\n ret string ==> \n %hs \n", _this->buf);
-        struct JRC jrc;
-        decodeJson(_this->buf, &jrc);
-
-        curl_easy_cleanup(hd);
+    if (ret != CURLE_OK) {
+      fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(ret));
     }
+
+    wprintf(L"\n ret string ==> \n %hs \n", _this->buf);
+    struct JRC jrc;
+    decodeJson(_this->buf, &jrc);
+
+    curl_easy_cleanup(hd);
+  }
 }
 
